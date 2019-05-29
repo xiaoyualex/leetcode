4038a58dd69cadbc4ea48fa9e553da2d5501c22e
xiaoyuzhu <alex349949507@gmail.com>
2019-05-29 11:08:31 -0400

    Create 3.py

---
+++ Dictionary/3.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        usedChar = {}
+        maxLength = start = 0
+        
+        for i in range(len(s)):
+            if s[i] in usedChar and start <= usedChar[s[i]]:
+                start = usedChar[s[i]] + 1
+            else:
+                maxLength = max(maxLength,i-start+1)
+                
+            usedChar[s[i]] = i
+            
+        return maxLength
