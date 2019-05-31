52839be72b605f662a5f0b1738ef7c13084a8e3f
xiaoyuzhu <alex349949507@gmail.com>
2019-05-31 16:08:09 -0400

    Create 102.py

---
+++ BFS/102.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        if not root:
+            return res
+        queue = [root]
+        while queue:
+            cur_level = []
+            size = len(queue)
+            while size > 0:
+                node = queue.pop(0)
+                size -= 1
+                cur_level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(cur_level)
+        return res
+    
