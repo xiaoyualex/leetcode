7454df854eda256d5f29bbcdf9d04b0ec409fd42
xiaoyuzhu <alex349949507@gmail.com>
2019-05-31 15:33:44 -0400

    Create 94.py

---
+++ DFS/94.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        if not root:
+            return res
+        self.helper(root,res)
+        return res
+    
+    def helper(self,node,res):
+        if node:
+            self.helper(node.left,res)
+            res.append(node.val)
+            self.helper(node.right,res)
