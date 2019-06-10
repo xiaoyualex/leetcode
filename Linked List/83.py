c0b40b4d71720df31b71d918635aed7e7087a642
xiaoyuzhu <alex349949507@gmail.com>
2019-06-10 11:25:38 -0400

    Update 83.py

--- Linked List/83.py
+++ Linked List/83.py
@@ -1,19 +1,15 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution(object):
     def deleteDuplicates(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
+        if not head or not head.next:
+            return head
+        
         cur = head
-        while cur:
-            while cur.next and cur.next.val == cur.val:
-                cur.next = cur.next.next     # skip duplicated node
-            cur = cur.next    
-        return head 
-      
+        while cur and cur.next:
+            while cur.next and cur.val == cur.next.val:
+                cur.next = cur.next.next
+            cur = cur.next
+        return head
