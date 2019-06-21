197a1a7fdda0b86426eefbbd588dc61731435fec
xiaoyuzhu <alex349949507@gmail.com>
2019-06-21 16:27:26 -0400

    Create 208.py

---
+++ Trie/208.py
@@ -0,0 +1,34 @@
+class TrieNode:
+# Initialize your data structure here.
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current.children:
+                current.children[letter] = TrieNode()
+            current = current.children[letter]
+        current.is_word = True
+
+    def search(self, word):
+        current = self.root
+        for letter in word:
+            current = current.children.get(letter)
+            if current is None:
+                return False
+        return current.is_word
+
+    def startsWith(self, prefix):
+        current = self.root
+        for letter in prefix:
+            current = current.children.get(letter)
+            if current is None:
+                return False
+        return True
